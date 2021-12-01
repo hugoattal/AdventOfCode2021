e9c745d09609543437d705ed5636025d88b542f2
Hugo ATTAL <hugoattal@hotmail.fr>
2021-12-01 16:16:04 +0100

    Don't juge me

--- day01/index.ts
+++ day01/index.ts
@@ -1,31 +1,25 @@
 function run1(data: Array<string>): number {
-    let previousMeasurement = 1000000;
     let increasedMeasurement = 0;
 
-    for (const line of data) {
-        const measurement = parseInt(line);
-        if (measurement > previousMeasurement) {
+    for (let i = 1; i < data.length; i++) {
+        if (parseInt(data[i]) > parseInt(data[i-1])) {
             increasedMeasurement++;
         }
-        previousMeasurement = measurement
     }
 
     return increasedMeasurement;
 }
 
 function run2(data: Array<string>): number {
-    let previousWindow = 1000000;
-    let increasedWindow = 0;
+    let increasedMeasurement = 0;
 
-    for (let i = 2; i < data.length; i++) {
-        const window = parseInt(data[i - 2]) + parseInt(data[i - 1]) + parseInt(data[i]);
-        if (window > previousWindow) {
-            increasedWindow++;
+    for (let i = 3; i < data.length; i++) {
+        if (parseInt(data[i]) > parseInt(data[i-3])) {
+            increasedMeasurement++;
         }
-        previousWindow = window
     }
 
-    return increasedWindow;
+    return increasedMeasurement;
 }
 
 export { run1, run2 }
