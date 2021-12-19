4bac0a681600dd23be89b1352a34767ad03662e7
Hugo <hugoattal@hotmail.fr>
2021-12-19 20:15:20 +0100

    Day 19

--- day19/index.ts
+++ day19/index.ts
@@ -1,5 +1,4 @@
 import * as _ from "lodash";
-import { clone } from "lodash";
 
 function run1(data: Array<string>): number {
     type TVector = {
@@ -8,13 +7,27 @@ function run1(data: Array<string>): number {
         z: number;
     }
 
-    type TTransform = {
-        translation: TVector,
-        rotation: TVector,
-        mirrored: boolean
+    type TOrientation = {
+        x: {
+            axis: string,
+            orientation: 1 | -1
+        },
+        y: {
+            axis: string,
+            orientation: 1 | -1
+        },
+        z: {
+            axis: string,
+            orientation: 1 | -1
+        }
     }
 
-    const scannerMap = {} as Record<number, Record<string, boolean>>
+    type TTransform = {
+        translation: TVector,
+        orientation: TOrientation
+    }
+
+    const scannerMap = {} as Record<string, Record<string, TVector>>;
 
     let currentScanner = 0;
 
@@ -29,7 +42,7 @@ function run1(data: Array<string>): number {
             continue;
         }
 
-        scannerMap[currentScanner][line] = true;
+        scannerMap[currentScanner][line] = stringToVector(line);
     }
 
     const map = scannerMap[0];
@@ -37,27 +50,79 @@ function run1(data: Array<string>): number {
 
     const transforms: Array<TTransform> = [];
 
-    for (let rotX = 0; rotX < 4; rotX++) {
-        for (let rotY = 0; rotY < 4; rotY++) {
-            for (let rotZ = 0; rotZ < 4; rotZ++) {
+    const axis = ["+x", "+y", "+z", "-x", "-y", "-z"];
+
+    axis.forEach((x) => {
+        axis.forEach((y) => {
+            if (x[1] === y[1]) {
+                return;
+            }
+            axis.forEach((z) => {
+                if (x[1] === z[1] || y[1] === z[1]) {
+                    return;
+                }
                 transforms.push({
-                    translation: {x: 0, y: 0, z: 0},
-                    rotation: {x: rotX, y: rotY, z: rotZ},
-                    mirrored: false
-                })
-                transforms.push({
-                    translation: {x: 0, y: 0, z: 0},
-                    rotation: {x: rotX, y: rotY, z: rotZ},
-                    mirrored: true
-                })
+                    translation: { x: 0, y: 0, z: 0 },
+                    orientation: {
+                        x: { axis: x[1], orientation: x[0] === "+" ? 1 : -1 },
+                        y: { axis: y[1], orientation: y[0] === "+" ? 1 : -1 },
+                        z: { axis: z[1], orientation: z[0] === "+" ? 1 : -1 }
+                    }
+                });
+            });
+        });
+    });
+
+    while (Object.keys(scannerMap).length > 0) {
+        console.log(Object.keys(scannerMap).length);
+        scannerSearch();
+    }
+
+    return Object.keys(map).length;
+
+    function scannerSearch() {
+        for (let scannerId of Object.keys(scannerMap)) {
+            for (let mapBeacon of Object.values(map)) {
+                const scannerKeys = Object.keys(scannerMap[scannerId]);
+                for (let matchKey = 0; matchKey < scannerKeys.length - 11; matchKey++) {
+                    const matchBeaconVector = scannerMap[scannerId][scannerKeys[matchKey]];
+
+                    for (let transform of transforms) {
+                        transform.translation = { x: 0, y: 0, z: 0 };
+                        const transformedMatch = transformVector(matchBeaconVector, transform);
+                        transform.translation = {
+                            x: mapBeacon.x - transformedMatch.x,
+                            y: mapBeacon.y - transformedMatch.y,
+                            z: mapBeacon.z - transformedMatch.z
+                        };
+
+                        let matchOrientation = 1;
+
+                        for (let otherKey = matchKey + 1; otherKey < scannerKeys.length; otherKey++) {
+                            const transformedString = vectorToString(transformVector(scannerMap[scannerId][scannerKeys[otherKey]], transform));
+                            if (map[transformedString]) {
+                                matchOrientation++;
+                            }
+                            if (scannerKeys.length - otherKey < 12 - matchOrientation) {
+                                break;
+                            }
+                        }
+
+                        if (matchOrientation >= 12) {
+                            mergeToMap(scannerId, transform);
+                            delete scannerMap[scannerId];
+                            return;
+                        }
+                    }
+                }
             }
         }
     }
 
-    while (Object.keys(scannerMap).length > 0) {
-        for (let scannerId of Object.keys(scannerMap)) {
-
-
+    function mergeToMap(scannerId: string, transform: TTransform) {
+        for (const beacon of Object.values(scannerMap[scannerId])) {
+            const transformedBeacon = transformVector(beacon, transform);
+            map[vectorToString(transformedBeacon)] = transformedBeacon;
         }
     }
 
@@ -66,7 +131,7 @@ function run1(data: Array<string>): number {
     }
 
     function stringToVector(string: string): TVector {
-        const split = string.split(",").map(parseInt);
+        const split = string.split(",").map((value) => parseInt(value));
         return {
             x: split[0],
             y: split[1],
@@ -74,34 +139,175 @@ function run1(data: Array<string>): number {
         };
     }
 
-    function transformVector(vector: TVector, transform: TTransform) {
-        const newVector = _.clone(vector);
-        if (transform.mirrored) {
-            newVector.x *= -1;
-        }
-
-        for (let rotX = 0; rotX < transform.rotation.x; rotX++) {
-            let temp = newVector.y;
-            newVector.y = newVector.z;
-            newVector.z = -temp;
-        }
-
-        for (let rotY = 0; rotY < transform.rotation.y; rotY++) {
-            let temp = newVector.x;
-            newVector.x = newVector.z;
-            newVector.z = -temp;
-        }
-
-        for (let rotZ = 0; rotZ < transform.rotation.z; rotZ++) {
-            let temp = newVector.x;
-            newVector.x = newVector.y;
-            newVector.y = -temp;
-        }
+    function transformVector(vector: TVector, transform: TTransform): TVector {
+        return {
+            x: vector[transform.orientation.x.axis as "x" | "y" | "z"] * transform.orientation.x.orientation + transform.translation.x,
+            y: vector[transform.orientation.y.axis as "x" | "y" | "z"] * transform.orientation.y.orientation + transform.translation.y,
+            z: vector[transform.orientation.z.axis as "x" | "y" | "z"] * transform.orientation.z.orientation + transform.translation.z
+        };
     }
 }
 
-function run2(_data: Array<string>): number {
-    return 0;
+function run2(data: Array<string>): number {
+    type TVector = {
+        x: number;
+        y: number;
+        z: number;
+    }
+
+    type TOrientation = {
+        x: {
+            axis: string,
+            orientation: 1 | -1
+        },
+        y: {
+            axis: string,
+            orientation: 1 | -1
+        },
+        z: {
+            axis: string,
+            orientation: 1 | -1
+        }
+    }
+
+    type TTransform = {
+        translation: TVector,
+        orientation: TOrientation
+    }
+
+    const scannerMap = {} as Record<string, Record<string, TVector>>;
+
+    let currentScanner = 0;
+
+    for (const line of data) {
+        if (line.startsWith("---")) {
+            currentScanner = parseInt(line.match(/[0-9]+/)?.[0] as string);
+            scannerMap[currentScanner] = {};
+            continue;
+        }
+
+        if (line.trim() === "") {
+            continue;
+        }
+
+        scannerMap[currentScanner][line] = stringToVector(line);
+    }
+
+    const map = scannerMap[0];
+    delete scannerMap[0];
+
+    const transforms: Array<TTransform> = [];
+
+    const axis = ["+x", "+y", "+z", "-x", "-y", "-z"];
+
+    axis.forEach((x) => {
+        axis.forEach((y) => {
+            if (x[1] === y[1]) {
+                return;
+            }
+            axis.forEach((z) => {
+                if (x[1] === z[1] || y[1] === z[1]) {
+                    return;
+                }
+                transforms.push({
+                    translation: { x: 0, y: 0, z: 0 },
+                    orientation: {
+                        x: { axis: x[1], orientation: x[0] === "+" ? 1 : -1 },
+                        y: { axis: y[1], orientation: y[0] === "+" ? 1 : -1 },
+                        z: { axis: z[1], orientation: z[0] === "+" ? 1 : -1 }
+                    }
+                });
+            });
+        });
+    });
+
+    const scannerLocations: Array<TVector> = [{ x: 0, y: 0, z: 0 }];
+
+    while (Object.keys(scannerMap).length > 0) {
+        console.log(Object.keys(scannerMap).length);
+        scannerSearch();
+    }
+
+    let maxDistance = 0;
+
+    for (let i=0; i<scannerLocations.length; i++) {
+        for (let j=i+1; j<scannerLocations.length; j++) {
+            const distance = Math.abs(scannerLocations[j].x-scannerLocations[i].x)
+                + Math.abs(scannerLocations[j].y-scannerLocations[i].y)
+                + Math.abs(scannerLocations[j].z-scannerLocations[i].z);
+
+            maxDistance = Math.max(distance, maxDistance);
+        }
+    }
+
+    return maxDistance;
+
+    function scannerSearch() {
+        for (let scannerId of Object.keys(scannerMap)) {
+            for (let mapBeacon of Object.values(map)) {
+                const scannerKeys = Object.keys(scannerMap[scannerId]);
+                for (let matchKey = 0; matchKey < scannerKeys.length - 11; matchKey++) {
+                    const matchBeaconVector = scannerMap[scannerId][scannerKeys[matchKey]];
+                    for (let transform of transforms) {
+                        transform.translation = { x: 0, y: 0, z: 0 };
+                        const transformedMatch = transformVector(matchBeaconVector, transform);
+                        transform.translation = {
+                            x: mapBeacon.x - transformedMatch.x,
+                            y: mapBeacon.y - transformedMatch.y,
+                            z: mapBeacon.z - transformedMatch.z
+                        };
+
+                        let matchOrientation = 1;
+
+                        for (let otherKey = matchKey + 1; otherKey < scannerKeys.length; otherKey++) {
+                            const transformedString = vectorToString(transformVector(scannerMap[scannerId][scannerKeys[otherKey]], transform));
+                            if (map[transformedString]) {
+                                matchOrientation++;
+                            }
+                            if (scannerKeys.length - otherKey < 12 - matchOrientation) {
+                                break;
+                            }
+                        }
+
+                        if (matchOrientation >= 12) {
+                            scannerLocations.push(transform.translation);
+                            mergeToMap(scannerId, transform);
+                            delete scannerMap[scannerId];
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    function mergeToMap(scannerId: string, transform: TTransform) {
+        for (const beacon of Object.values(scannerMap[scannerId])) {
+            const transformedBeacon = transformVector(beacon, transform);
+            map[vectorToString(transformedBeacon)] = transformedBeacon;
+        }
+    }
+
+    function vectorToString(vector: TVector): string {
+        return `${vector.x},${vector.y},${vector.z}`;
+    }
+
+    function stringToVector(string: string): TVector {
+        const split = string.split(",").map((value) => parseInt(value));
+        return {
+            x: split[0],
+            y: split[1],
+            z: split[2]
+        };
+    }
+
+    function transformVector(vector: TVector, transform: TTransform): TVector {
+        return {
+            x: vector[transform.orientation.x.axis as "x" | "y" | "z"] * transform.orientation.x.orientation + transform.translation.x,
+            y: vector[transform.orientation.y.axis as "x" | "y" | "z"] * transform.orientation.y.orientation + transform.translation.y,
+            z: vector[transform.orientation.z.axis as "x" | "y" | "z"] * transform.orientation.z.orientation + transform.translation.z
+        };
+    }
 }
 
 export { run1, run2 };
